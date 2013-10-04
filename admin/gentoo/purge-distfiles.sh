9555f0c0858d03cda8d86fa095f42a5278c5f4dc
laomaiweng <quentin.minster@gmail.com>
2013-10-04 14:04:25 +0200

    Modified and renamed admin/gentoo/purge-distfiles.sh to admin/gentoo/purge-distfiles

--- admin/gentoo/purge-distfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Source Portage config
-. /etc/portage/make.conf
-
-# Delete everything (except dotfiles) in the distfiles directory
-echo "  In $DISTIR:"
-rm -RI ${DISTDIR:-/usr/portage/distfiles}/*
-
